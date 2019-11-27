b2bdb389551e6ed09ee830475d260b3db1fe9196
Luna <Vanbockryck.Luna@gmail.com>
2019-11-27 16:04:16 +0100

    restructured calc functions

--- resources/js/calc.js
+++ resources/js/calc.js
@@ -1,36 +1,43 @@
 /* ----- ALL DAMAGE CALCULATIONS ----- */
-
 function calcDamage() {
+
     /* ----- EVASION CHANCE (DODGE CHANCE) -----*/
     /*  generates number from 1-100
         if generated number is equal or smaller than the evasion stat
         attack/skill will be dodged
     */
-    function calcEvasion() {
-        let evasion = itemStats.evasion;
-        let dodgeChance = Math.floor(Math.random() * (100 - 1 + 1)) + 1; //RNG 1-100
-        if (dodgeChance > 0 && dodgeChance <= evasion) {
-            console.log("evasion rate from item: " + evasion + "\n" + "RNG num: " + dodgeChance + "\n" + "dodged!");
-        } else {
-            console.log("evasion rate from item: " + evasion + "\n" + "RNG num: " + dodgeChance + "\n" + "got hit");
-        }
-    }
+
+    function calcEvasion() {}
+
     /* ----- ATTACK CHANCE (CHANCE TO HIT TWICE) -----*/
     /*  generates number from 1-100
         if generated number is equal or smaller than the attack chance stat
-        attack will hit twice (calculated later)
+        attack will hit twice (double hit multiplier)
     */
-    function calcDoubleHit() {
-        let atkChance = itemStats.attackChance;
-        let atkDoubleHitChance = Math.floor(Math.random() * (100 - 1 + 1)) + 1; //RNG 1-100
-        if (atkDoubleHitChance > 0 && atkDoubleHitChance <= atkChance) { //RNG 1 until evasion value = dodged
-            doubleHit = 2;
-        } else {
-            doubleHit = 1;
-        }
-    }
 
+    function calcDoubleHit() {}
 
+    /* ----- ELEMENT CALCULATIONS -----*/
+    /*  
+        These elements do double damage to each other
+                FIRE <==> ICE
+                WIND <==> ELEC
+                NUKE <==> PSY
+                BLESS <==> CURSE
+    */
+
+    function calcElement() {}
+
+    /* ----- FINAL DAMAGE CALCULATIONS -----*/
+    /*  
+       normal damage = base weapon damage - enemy resistance
+       buffed damage = normal damage + buff
+       elemental damage = buffed damage * elemental multiplier
+       final damage = elemental damage * double hit multiplier 
+       actual damage = final damage with dodge check
+    */
+
+    function calcFinalDamage() {}
 
 
 
